bb61e4f6ccd53274bac49c77e95fd09aa27880dc
Gary Danko <gdanko@protonmail.com>
2024-11-27 11:55:02 -0800

    Remove the init logic from root

--- cmd/root.go
+++ cmd/root.go
@@ -1,22 +1,16 @@
 package cmd
 
 import (
-	"encoding/json"
-	"fmt"
-
-	"github.com/gdanko/clearsky/globals"
-	"github.com/gdanko/clearsky/pkg/api"
 	"github.com/spf13/cobra"
 )
 
 var (
 	accountName       string
+	err               error
 	showBlockingUsers bool
-	showBlockList     bool
-	showListCount     bool
-	showListNames     bool
-	userId            string
-	rootCmd           = &cobra.Command{
+	// showListNames     bool
+	userId  string
+	rootCmd = &cobra.Command{
 		Use:   "clearsky",
 		Short: "clearsky is a command line interface for the clearsky.app API",
 		Long:  "clearsky is a command line interface for the clearsky.app API",
@@ -27,19 +21,7 @@ func Execute() error {
 	return rootCmd.Execute()
 }
 
-func init() {
-	rootCmd.PersistentFlags().StringVarP(&accountName, "account", "a", "", "The BlueSky account name.")
+// func init() {
 
-	url := fmt.Sprintf("https://api.clearsky.services/api/v1/anon/get-did/%s", accountName)
-	body, err := api.FetchUrl(url)
-	if err != nil {
-		panic(err)
-	}
-
-	getDid := globals.GetUserData()
-	err = json.Unmarshal(body, &getDid)
-	if err != nil {
-		panic(err)
-	}
-	userId = getDid.Data.DidIdentifier
-}
+// 	return
+// }
