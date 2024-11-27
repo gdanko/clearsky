3ee4a3efe776a5f0c16aaf5fe26b155cb0709e67
Gary Danko <gdanko@protonmail.com>
2024-11-27 11:54:40 -0800

    Add worker struct and remove unused funcs

--- globals/globals.go
+++ globals/globals.go
@@ -1,9 +1,5 @@
 package globals
 
-import (
-	"sync"
-)
-
 // User's did
 type UserData struct {
 	AvatarUrl     string `json:"avatar_url"`
@@ -60,49 +56,7 @@ type BlueSkyUser struct {
 	Posts          int    `json:"postsCount"`
 }
 
-var (
-	mu              sync.RWMutex
-	blockListOutput BlockListOutput
-	blueSkyUser     BlueSkyUser
-	userDid         UserDid
-)
-
-// set and get pairs
-func SetBlockListOutput(x BlockListOutput) {
-	mu.Lock()
-	blockListOutput = x
-	mu.Unlock()
-}
-
-func GetBlockListOutput() (x BlockListOutput) {
-	mu.Lock()
-	x = blockListOutput
-	mu.Unlock()
-	return x
-}
-
-func SetBlueSkyUser(x BlueSkyUser) {
-	mu.Lock()
-	blueSkyUser = x
-	mu.Unlock()
-}
-
-func GetBlueSkyUser() (x BlueSkyUser) {
-	mu.Lock()
-	x = blueSkyUser
-	mu.Unlock()
-	return x
-}
-
-func SetUserData(x UserDid) {
-	mu.Lock()
-	userDid = x
-	mu.Unlock()
-}
-
-func GetUserData() (x UserDid) {
-	mu.Lock()
-	x = userDid
-	mu.Unlock()
-	return x
+// Concurrent worker job
+type Job struct {
+	URL string
 }
