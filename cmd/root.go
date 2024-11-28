e0bccba8bcf0261968dea588fcb77c52a5b78b17
gdanko <Gary_Danko@intuit.com>
2024-11-28 11:43:31 -0800

    add more vars here

--- cmd/root.go
+++ cmd/root.go
@@ -5,24 +5,25 @@ import (
 )
 
 var (
-	accountName       string
-	debugFlag         bool
-	err               error
-	showBlockingUsers bool
-	// showListNames     bool
-	userId  string
-	rootCmd = &cobra.Command{
+	accountName           string
+	batchOperationTimeout int
+	batchChunkSize        int
+	debugFlag             bool
+	err                   error
+	listMaxResults        int
+	showBlockingUsers     bool
+	showListNames         bool
+	displayName           string
+	userId                string
+	rootCmd               = &cobra.Command{
 		Use:   "clearsky",
-		Short: "clearsky is a command line interface for the clearsky.app API",
-		Long:  "clearsky is a command line interface for the clearsky.app API",
+		Short: "clearsky is a command line interface for the clearsky.services API.",
+		Long:  "clearsky is a command line interface for the clearsky.services API.",
 	}
 )
 
 func Execute() error {
+	rootCmd.PersistentFlags().BoolVarP(&debugFlag, "debug", "d", false, "Enable debugging output.")
+
 	return rootCmd.Execute()
 }
-
-// func init() {
-
-// 	return
-// }
