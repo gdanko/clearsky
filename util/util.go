9fb9857ef1a51243481f92d11445071167b522a1
gdanko <Gary_Danko@intuit.com>
2024-11-28 13:34:25 -0800

    Add logging stuff

--- util/util.go
+++ util/util.go
@@ -1,7 +1,16 @@
 package util
 
-import "github.com/gdanko/clearsky/globals"
+import (
+	"os"
+	"sort"
+	"strings"
 
+	"github.com/gdanko/clearsky/globals"
+	"github.com/sirupsen/logrus"
+	prefixed "github.com/x-cray/logrus-prefixed-formatter"
+)
+
+// Take the list of blocking users and split them into chunks
 func SliceChunker(input []globals.BlockingUser, chunkSize int) (output [][]globals.BlockingUser) {
 	for i := 0; i < len(input); i += chunkSize {
 		end := i + chunkSize
@@ -11,4 +20,37 @@ func SliceChunker(input []globals.BlockingUser, chunkSize int) (output [][]globa
 		output = append(output, input[i:end])
 	}
 	return output
+}
+
+// ReturnLogLevels : Return a comma-delimited list of log levels
+func ReturnLogLevels(levelMap map[string]logrus.Level) string {
+	logLevels := make([]string, 0, len(levelMap))
+	for k := range levelMap {
+		logLevels = append(logLevels, k)
+	}
+	sort.Strings(logLevels)
+
+	return strings.Join(logLevels, ", ")
+}
+
+// ConfigureLogger : Configure the logger
+func ConfigureLogger(logLevel logrus.Level, nocolorFlag bool) (logger *logrus.Logger) {
+	disableColors := false
+	if nocolorFlag {
+		disableColors = true
+	}
+	logger = &logrus.Logger{
+		Out:   os.Stderr,
+		Level: logLevel,
+		Formatter: &prefixed.TextFormatter{
+			DisableColors:    disableColors,
+			DisableTimestamp: true,
+			TimestampFormat:  "2006-01-02 15:04:05",
+			FullTimestamp:    true,
+			ForceFormatting:  false,
+		},
+	}
+	logger.SetLevel(logLevel)
+
+	return logger
 }
