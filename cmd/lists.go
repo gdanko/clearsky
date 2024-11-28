5e67f09ffb3284548e8c53ede8a5b2424d859acb
gdanko <Gary_Danko@intuit.com>
2024-11-28 11:43:52 -0800

    Start populating lists

--- cmd/lists.go
+++ cmd/lists.go
@@ -1,27 +1,45 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+
+	"github.com/gdanko/clearsky/globals"
+	"github.com/gdanko/clearsky/pkg/api"
+	"github.com/spf13/cobra"
+)
 
 var (
 	listsCmd = &cobra.Command{
 		Use:          "lists",
-		Short:        "Display the number of moderated lists --account is a member of",
-		Long:         "Display the number of moderated lists --account is a member of",
-		PreRun:       listsPreRunCmd,
-		Run:          listsRunCmd,
+		Short:        "Display the number of moderated lists --account is a member of.",
+		Long:         "Display the number of moderated lists --account is a member of.",
+		PreRunE:      listsPreRunCmd,
+		RunE:         listsRunCmd,
 		SilenceUsage: true,
 	}
 )
 
 func init() {
-	listsCmd.PersistentFlags().BoolVarP(&showBlockingUsers, "list-names", "n", false, "Gather the list of moderated lists' names (expensive).")
+	GetListsFlags(listsCmd)
 	rootCmd.AddCommand(listsCmd)
 }
 
-func listsPreRunCmd(cmd *cobra.Command, args []string) {
-	// Empty
+func listsPreRunCmd(cmd *cobra.Command, args []string) error {
+	globals.SetDebugFlag(debugFlag)
+	if accountName != "" {
+		displayName, userId, err = api.GetUserID(accountName)
+		if err != nil {
+			return err
+		}
+	} else {
+		fmt.Println("The required --account flag is missing")
+		cmd.Help()
+		os.Exit(1)
+	}
+	return nil
 }
 
-func listsRunCmd(cmd *cobra.Command, args []string) {
-	// Empty
+func listsRunCmd(cmd *cobra.Command, args []string) error {
+	return nil
 }
