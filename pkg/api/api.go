28df374268dc241575895046752aa4cb2960add1
gdanko <Gary_Danko@intuit.com>
2024-11-29 12:27:32 -0800

    Simplify the API handling

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -9,8 +9,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/forPelevin/gomoji"
 	"github.com/gdanko/clearsky/globals"
+	"github.com/gdanko/clearsky/util"
 	"github.com/sirupsen/logrus"
 	"github.com/useinsider/go-pkg/insrequester"
 	// "golang.org/x/sync/errgroup"
@@ -54,7 +54,7 @@ func GetUserID(accountName string, logger *logrus.Logger) (displayName string, u
 
 func worker(requester *insrequester.Request, jobs <-chan globals.Job, results chan<- *http.Response, wg *sync.WaitGroup, logger *logrus.Logger) {
 	for job := range jobs {
-		logger.Debug(job.URL)
+		logger.Debugf("Fetching %s", job.URL)
 		res, err := requester.Get(insrequester.RequestEntity{Endpoint: job.URL})
 		if err != nil {
 			fmt.Println(err)
@@ -64,83 +64,34 @@ func worker(requester *insrequester.Request, jobs <-chan globals.Job, results ch
 	}
 }
 
-func GetBlockingUsersList(userId string, logger *logrus.Logger) (blockListOutput globals.BlockListOutput, err error) {
+func ExpandBlockListUsers(ids []string, batchOperationTimeout int, logger *logrus.Logger) (blockListUsers []globals.BlueSkyUser, err error) {
+	// https://medium.com/insiderengineering/concurrent-http-requests-in-golang-best-practices-and-techniques-f667e5a19dea
 	var (
-		blockListPage globals.BlockListPage
-		body          []byte
-		maxPages      = 1000
-		url           string
-	)
-
-	// https://api.clearsky.services/api/v1/anon/single-blocklist/did:plc:ccskhvd467uwdrxpwaudnbni
-	url = fmt.Sprintf("https://api.clearsky.services/api/v1/anon/single-blocklist/%s", userId)
-	body, err = FetchUrl(url, logger)
-	if err != nil {
-		return globals.BlockListOutput{}, err
-	}
-
-	blockListPage = globals.BlockListPage{}
-	err = json.Unmarshal(body, &blockListPage)
-	if err != nil {
-		return globals.BlockListOutput{}, err
-	}
-	if len(blockListPage.Data.Blocklist) > 0 {
-		blockListOutput.Items = append(blockListOutput.Items, blockListPage.Data.Blocklist...)
-	} else {
-		return globals.BlockListOutput{}, err
-	}
-
-	// Now we cycle through /2, /3, etc until there are no more
-	// https://api.clearsky.services/api/v1/anon/single-blocklist/did:plc:ccskhvd467uwdrxpwaudnbni/2
-	if len(blockListOutput.Items) >= 100 {
-		for i := 2; i <= maxPages; i++ {
-			url = fmt.Sprintf("https://api.clearsky.services/api/v1/anon/single-blocklist/%s/%d", userId, i)
-			body, err = FetchUrl(url, logger)
-			if err != nil {
-				return globals.BlockListOutput{}, err
-			}
-			blockListPage = globals.BlockListPage{}
-			err = json.Unmarshal(body, &blockListPage)
-			if err != nil {
-				panic(err)
-			}
-			if len(blockListPage.Data.Blocklist) > 0 {
-				blockListOutput.Items = append(blockListOutput.Items, blockListPage.Data.Blocklist...)
-			} else {
-				break
-			}
-		}
-	}
-	return blockListOutput, nil
-}
-
-func ExpandBlockListUsers(blockList *[]globals.BlockingUser, batchOperationTimeout int, logger *logrus.Logger) (err error) {
-	var (
-		blockingUser globals.BlockingUser
-		userObject   globals.BlueSkyUser
-		url          string
-		requester    *insrequester.Request
-		workerCount  = 100
-		wg           sync.WaitGroup
+		id          string
+		userObject  globals.BlueSkyUser
+		url         string
+		requester   *insrequester.Request
+		workerCount = 100
+		wg          sync.WaitGroup
 	)
 	requester = insrequester.NewRequester().Load()
 	requester.WithTimeout(time.Duration(batchOperationTimeout) * time.Second)
-	jobs := make(chan globals.Job, len(*blockList))
-	results := make(chan *http.Response, len(*blockList))
+	jobs := make(chan globals.Job, len(ids))
+	results := make(chan *http.Response, len(ids))
 
 	for w := 0; w < workerCount; w++ {
 		go worker(requester, jobs, results, &wg, logger)
 	}
 
-	wg.Add(len(*blockList))
-	for _, blockingUser = range *blockList {
-		url = fmt.Sprintf("https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=%s", (blockingUser.DID))
+	wg.Add(len(ids))
+	for _, id = range ids {
+		url = fmt.Sprintf("https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=%s", id)
 		jobs <- globals.Job{URL: url}
 	}
 	close(jobs)
 	wg.Wait()
 
-	for i := 0; i < len(*blockList); i++ {
+	for i := 0; i < len(ids); i++ {
 		resp := <-results
 		if resp != nil {
 			body, err := io.ReadAll(resp.Body)
@@ -155,16 +106,97 @@ func ExpandBlockListUsers(blockList *[]globals.BlockingUser, batchOperationTimeo
 			if err != nil {
 				panic(err)
 			}
-			(*blockList)[i].Username = userObject.Handle
-			(*blockList)[i].DisplayName = gomoji.RemoveEmojis(userObject.DisplayName)
-			(*blockList)[i].Description = userObject.Description
-			(*blockList)[i].Banner = userObject.Banner
-			(*blockList)[i].FollowsCount = userObject.FollowsCount
-			(*blockList)[i].FollowersCount = userObject.FollowersCount
-			(*blockList)[i].Posts = userObject.Posts
-		} else {
-			fmt.Println("Ouch! nil response")
+			blockListUsers = append(blockListUsers, userObject)
+		}
+	}
+	return blockListUsers, err
+}
+
+func ProcessBlockingUsersList(blockingList *map[string]globals.BlockingUser, blockListPage globals.BlockListPage, batchChunkSize int, batchOperationTimeout int, logger *logrus.Logger) (err error) {
+	var (
+		chunk   []string
+		didSet  []string
+		divided [][]string
+		i       int
+		temp    globals.BlockingUser
+	)
+	for _, blockingUser := range blockListPage.Data.Blocklist {
+		(*blockingList)[blockingUser.DID] = globals.BlockingUser{DID: blockingUser.DID, BlockedDate: blockingUser.BlockedDate}
+		didSet = append(didSet, blockingUser.DID)
+	}
+	divided = util.SliceChunker(didSet, batchChunkSize)
+	for i, chunk = range divided {
+		logger.Debugf("Processing chunk %d of %d", i+1, len(divided))
+		blockListUsers, err := ExpandBlockListUsers(chunk, batchOperationTimeout, logger)
+		if err != nil {
+			return err
+		}
+		for _, blockListUser := range blockListUsers {
+			temp = (*blockingList)[blockListUser.DID]
+
+			temp.Banner = blockListUser.Banner
+			temp.Description = blockListUser.Description
+			temp.DisplayName = blockListUser.DisplayName
+			temp.Error = blockListUser.Error
+			temp.FollowersCount = blockListUser.FollowersCount
+			temp.FollowsCount = blockListUser.FollowsCount
+			temp.Labels = blockListUser.Labels
+			temp.Message = blockListUser.Message
+			temp.PinnedPost = blockListUser.PinnedPost
+			temp.Posts = blockListUser.Posts
+			temp.Username = blockListUser.Handle
+
+			(*blockingList)[blockListUser.DID] = temp
 		}
 	}
 	return nil
+}
+
+func GetBlockingUsersList(userId string, batchChunkSize int, batchOperationTimeout int, logger *logrus.Logger) (blockingList map[string]globals.BlockingUser, err error) {
+	// Get the user info BEFORE inserting into the database...
+	// When you have each batch of 100, go get their details
+	var (
+		blockListPage globals.BlockListPage
+		body          []byte
+		maxPages      = 3
+		url           string
+	)
+
+	blockingList = make(map[string]globals.BlockingUser)
+
+	// https://api.clearsky.services/api/v1/anon/single-blocklist/did:plc:ccskhvd467uwdrxpwaudnbni
+	url = fmt.Sprintf("https://api.clearsky.services/api/v1/anon/single-blocklist/%s", userId)
+	body, err = FetchUrl(url, logger)
+	if err != nil {
+		return blockingList, err
+	}
+	blockListPage = globals.BlockListPage{}
+	err = json.Unmarshal(body, &blockListPage)
+	if err != nil {
+		return blockingList, err
+	}
+	err = ProcessBlockingUsersList(&blockingList, blockListPage, batchChunkSize, batchOperationTimeout, logger)
+	if err != nil {
+		return blockingList, err
+	}
+
+	if len(blockingList) >= 100 {
+		for i := 2; i <= maxPages; i++ {
+			url = fmt.Sprintf("https://api.clearsky.services/api/v1/anon/single-blocklist/%s/%d", userId, i)
+			body, err = FetchUrl(url, logger)
+			if err != nil {
+				return blockingList, err
+			}
+			blockListPage = globals.BlockListPage{}
+			err = json.Unmarshal(body, &blockListPage)
+			if err != nil {
+				return blockingList, err
+			}
+			err = ProcessBlockingUsersList(&blockingList, blockListPage, batchChunkSize, batchOperationTimeout, logger)
+			if err != nil {
+				return blockingList, err
+			}
+		}
+	}
+	return blockingList, nil
 }
