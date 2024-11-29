02260d6a0c65eb2f78cd7473a87d9fe1dc6ed4ee
gdanko <Gary_Danko@intuit.com>
2024-11-29 12:25:07 -0800

    Add the version command

---
+++ cmd/version.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/gdanko/clearsky/pkg/clearsky"
+	"github.com/spf13/cobra"
+)
+
+var (
+	versionCmd = &cobra.Command{
+		Use:          "version",
+		Short:        "Print the current enpass version",
+		Long:         "Print the current enpass version",
+		RunE:         runVersionCmd,
+		SilenceUsage: true,
+	}
+)
+
+func init() {
+	// This is only here to override the required vault flag for other commands
+	versionCmd.Flags().BoolVarP(&versionFull, "full", "f", false, "Display more version information")
+	rootCmd.AddCommand(versionCmd)
+}
+
+func runVersionCmd(cmd *cobra.Command, args []string) error {
+	fmt.Fprintf(cmd.OutOrStdout(), "%s\n", clearsky.Version(true, true, versionFull))
+
+	return nil
+}
