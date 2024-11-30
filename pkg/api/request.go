507afef312536445debe13046a65040d953e32c7
gdanko <Gary_Danko@intuit.com>
2024-11-30 11:15:13 -0800

    Allow POST and POST data

--- pkg/api/request.go
+++ pkg/api/request.go
@@ -1,22 +1,39 @@
 package api
 
 import (
+	"bytes"
 	"io"
 	"net/http"
 
 	"github.com/sirupsen/logrus"
 )
 
-func FetchUrl(url string, logger *logrus.Logger) (body []byte, err error) {
+func FetchUrl(method string, url string, logger *logrus.Logger, payload []byte) (body []byte, err error) {
 	logger.Debugf("Fetching %s", url)
-	resp, err := http.Get(url)
+	client := &http.Client{}
+
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(payload))
 	if err != nil {
-		return []byte{}, err
+		return body, err
+	}
+
+	if payload != nil {
+		req.Header.Set("Content-Type", "application/json")
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return body, err
+	}
+
+	if err != nil {
+		return body, err
 	}
 	defer resp.Body.Close()
+
 	body, err = io.ReadAll(resp.Body)
 	if err != nil {
-		return []byte{}, err
+		return body, err
 	}
 	return body, nil
 }
